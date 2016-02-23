b9897877a4f40a1af2410483d5433e4c5c5605a0
Alexander Parsan <alexanderparsan28@gmail.com>
2016-02-23 17:07:54 -0500

    Create input.py

---
+++ Lesson1/input.py
@@ -0,0 +1,10 @@
+#this is the best way to get Input from a user and display it(print it out) on the screen. 
+
+textinput = raw_input("Please enter some text:") #Get text from a user
+numberinput = input("Please enter a number:") #Get a integer or decimal from  a user
+
+numberinput = numberinput + 2 
+
+print textinput
+print numberinput 
+
