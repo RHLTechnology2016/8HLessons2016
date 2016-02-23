e2f62ad3e8467415ed912439e99a001356d1a9b3
Alexander Parsan <alexanderparsan28@gmail.com>
2016-02-23 17:13:32 -0500

    Create Math.py

---
+++ Lesson1/Math.py
@@ -0,0 +1,18 @@
+#Here are just a few of the basic mathamatical operations in Python
+
+add = 9 + 10 #Adds9 and 10
+subtract = 96 - 42 #Subtracts 42 from 96
+multiply = 20 * 20 #Multiplys 20 by 20
+divide = 90 /2 #Divides 90 by 2
+exponent = 90 ** 3 #90 * 90 * 90
+
+#These can be put together just like in Algebra
+
+MyNumbers = 100 * 5 + 2 / 6 
+
+#You can also add in variables
+
+number = input("Give me a number:")
+MyNumbers2 = 200 * number + 2 - number
+
+#There is a lot more Math That Can be done in Python besides all of this, but we will go over those a little later!
