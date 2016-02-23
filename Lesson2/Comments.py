bc993e52270c304687476279d7f3e6c1361774e9
Alexander Parsan <alexanderparsan28@gmail.com>
2016-02-23 18:56:09 -0500

    Create Comments.py

---
+++ Lesson2/Comments.py
@@ -0,0 +1,47 @@
+#I've been using comments, on the Github in this Repo and when uploaded some of your code.
+#In fact, I'm using comments write now. 
+#From now on, we should leave comments in our code,
+#First of all, comments are ignored by the computer. 
+#To make a comment, do what I am doing. Just add a 
+#to the start of any line. Python will ignore anything I say. 
+
+#Comments at the start of a program, to give the Author's Name, Program Name, If The Program is Free To Share and Redistribute or if you can't do any of that.
+#here is an example:
+
+
+#Comments.py
+#Short Program to show how comments are used in Python. 
+#Program Written By: Alexander Parsan
+#Under the GPL v3.0
+
+#Comments can also be used during debugging. 
+#For example, if you want a line not to run during debugging simply add a # in front of it
+
+#Putting comments before sections help describe it! 
+#print "This line will not print"
+print "This line will print" #Putting a Comment after a line is a good way to add more info about it. 
+
+
+#Another Type of Comments
+#If you need to remove a large amount of code from your program, I suggest you use Tripe Quoted Strings. These are not an offical way to comment. Some do not think this is a good method, but there is nothing wrong with it!
+#Wrap """ around the text you want to comment out
+
+print "This stuff is not commented out"
+
+"""
+print "this stuff will not show up
+print "yup"
+print "9 + 10"
+print "HA"
+print "Goteem"
+"""
+
+
+#Times to Use Comments
+#It is a bad idea to use comments during contests, like the CCC and excessivily in Small programs. 
+#Use comments if you need to explain somethng you are doing 
+#I personally, will use comments throughout these lessons at the start, and sometimes throughout 
+#Comments are perhaps best used for Debugging
+#Use them a lot to remove code that you think is causing a problem. 
+#You might not need to do this now, but you will later!
+
