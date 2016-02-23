2a6e0256af6938017e9340b9c7ab473dbdf888c7
Alexander Parsan <alexanderparsan28@gmail.com>
2016-02-23 16:56:12 -0500

    Create variables.py

---
+++ Lesson1/variables.py
@@ -0,0 +1,21 @@
+#This program shows how to variables work
+#Remember how a variable works, VariableName, then an equal sign, then what you are assigning to the variable. Variables do not have quotes. Quotes represent strings. 
+#For example:
+
+a = 9  #Assigns a value of 9 to the variable a. 
+a = 10 #Changes the value a = 9 to equal 10(a = 10)
+b = 12 #Assgins a value of 12 to the variable b.
+c = a + b #Assigns a value of a + b(or 10 + 12) to the variable c. 
+
+#Does not display "A" or "B" or "C" on the screen. Instead it prints out the values assigned to A,B,C
+print a #Will show 10 on the screen
+print b #Will show 12 on the screen 
+print c #will show 22 on the screen
+
+#Variables can also equal text as well
+
+MyText = "Hello, my name is Alexander Parsan. I am a grade 8 Student at RHL." 
+
+print MyText #Displays "Hello, my name is Alexander Parsan. I am a grade 8 Student at RHL." 
+
+
