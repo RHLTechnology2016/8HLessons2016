e8862e91ae8e671306de05aed3ee42b13cd99f37
Alexander Parsan <alexanderparsan28@gmail.com>
2016-03-04 15:49:52 -0500

    Update and rename Loops.py to Loops2.py

--- Lesson2/Loops2.py
+++ Lesson2/Loops2.py
@@ -1,31 +1,9 @@
-#Loops.py
+#Loops2.py
 #Short Program to show how loops are used in Python. 
 #Program Written By: Alexander Parsan
 #Parts of Program By: Warren Sande, 2009 from "Hello world"
 # Released under MIT license   http://www.opensource.org/licenses/mit-license.php
 
-#Loops help us repeat code over and over again. 
-
-#Instead of doing this
-print "hello"
-print "hello"
-print "hello"
-print "hello"
-print "hello"
-
-print "Now let's try using a For Loop"
-#Below prints Hello 5 times
-for looper in [1, 2, 3, 4, 5]:
-    print "hello"
-    
-#If you have want to loop more than 10 times, a shortcut is to use range()
-
-for looper in range (1, 100):
-    print looper, "times 8 =", looper * 8 #Prints nth time we have looped * 8 
-    
-#You can also go through a list like this:
-for cool_guy in ["Spongebob", "Spiderman", "Justin Timberlake", "My dad"]:
-    print cool_guy, "is the coolest guy ever!"
     
 #while loops will continue executing the code as long as the condition is true
 
